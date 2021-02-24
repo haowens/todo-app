3d2af10a72fa62e7497e754b2415e6ab534c9ca8
Hayley Owens <haowens@email.unc.edu>
2021-02-24 12:23:12 -0500

    app take 3

--- src/components/task.js
+++ src/components/task.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Task = ({ name }) => {
   return (
-    <div>
+    <div key={name}>
       {/**
        * TODO: add an html or bootstrap element that uses the name variable to display an individual task
        */}
